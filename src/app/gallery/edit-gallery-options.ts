a61595736f226365ac8dc3b476ec74ad00162642
Patryk <patryk.leczycki1@gmail.com>
2020-11-26 14:31:45 +0100

    added photo model and extracted gallery options

---
+++ src/app/gallery/edit-gallery-options.ts
@@ -0,0 +1,35 @@
+// import {NgxGalleryAnimation, NgxGalleryImageSize, NgxGalleryOptions} from 'ngx-gallery-9';
+//
+// export const editGalleryOptions: NgxGalleryOptions[] = [
+//   {
+//     width: '600px',
+//     height: '100px',
+//     thumbnailsColumns: 5,
+//     imageAnimation: NgxGalleryAnimation.Slide,
+//     thumbnails: true,
+//     previewCloseOnEsc: true,
+//     previewCloseOnClick: true,
+//     previewKeyboardNavigation: true,
+//     previewInfinityMove: true,
+//     previewAnimation: false,
+//     imageInfinityMove: true,
+//     image: false,
+//     thumbnailSize: NgxGalleryImageSize.Contain,
+//     imageDescription: true,
+//     thumbnailActions: [{icon: 'fa fa-times-circle', onClick: this.deleteImage.bind(this), titleText: 'delete'}]
+//   },
+//   {
+//     breakpoint: 800,
+//     width: '100%',
+//     height: '600px',
+//     imagePercent: 80,
+//     thumbnailsPercent: 20,
+//     thumbnailsMargin: 20,
+//     thumbnailMargin: 20
+//   },
+//   // max-width 400
+//   {
+//     breakpoint: 400,
+//     preview: false
+//   }
+// ];
